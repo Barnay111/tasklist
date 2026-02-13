cfb210d81cf261c2a508f951fda6e6137e8b95a9
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2026-02-14 00:28:47 +0500

    Initial commit

---
+++ tasklist/src/main/java/com/example/tasklist/security/JwtEntityFactory.java
@@ -0,0 +1,29 @@
+package com.example.tasklist.security;
+
+import com.example.tasklist.domain.user.Role;
+import com.example.tasklist.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JwtEntityFactory {
+
+    public static JwtEntity create(User user) {
+        return new JwtEntity(
+                user.getId(),
+                user.getUsername(),
+                user.getName(),
+                user.getPassword(),
+                mapToGrantedAuthorities(new ArrayList<>(user.getRoles()))
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> roles) {
+        return roles.stream()
+                .map()
+    }
+
+
+}
