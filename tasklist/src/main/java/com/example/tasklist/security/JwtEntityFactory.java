a051f974c92c11fcf4ff887b1c6e9e54b7b1cb40
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2026-02-14 16:55:16 +0500

    Security

--- tasklist/src/main/java/com/example/tasklist/security/JwtEntityFactory.java
+++ tasklist/src/main/java/com/example/tasklist/security/JwtEntityFactory.java
@@ -3,10 +3,12 @@ package com.example.tasklist.security;
 import com.example.tasklist.domain.user.Role;
 import com.example.tasklist.domain.user.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JwtEntityFactory {
 
@@ -22,7 +24,9 @@ public class JwtEntityFactory {
 
     private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> roles) {
         return roles.stream()
-                .map()
+                .map(Enum::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
 
